19007a54d8bbb5cdad239659f08dbd2a658993a4
Rajkumar <rajkumar.patel@seagate.com>
2021-01-06 14:03:29 +0530

    head and tail phase of recursion

---
+++ Recursion/rec_ascending_print.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void foo( int n ) {
+   if ( n > 0 )
+   {
+      printf( "%d ", n );
+      printf( "call will be made now!\n" );
+      foo( n - 1 );
+   }
+}
+
+int main( ) {
+   int n = 3;
+
+   printf( "This program will print number while ascending phase\n" );
+   foo( n );
+}
