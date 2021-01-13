850c7179c0b1d365495fe55153155892c82fb83c
Rajkumar <rajkumar.patel@seagate.com>
2021-01-13 16:29:32 +0530

    Add palindrom using recursion

---
+++ Recursion/rec_palindrom_new.c
@@ -0,0 +1,37 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+bool isPalindrom( unsigned int num, int len ) {
+   if ( len <= 1 )
+   {
+      return true;
+   }
+
+   int          a, b;
+   unsigned int modtemp;
+   a       = num / ( int )( pow( 10, ( len - 1 ) ) );
+   modtemp = ( unsigned int )( pow( 10, ( len - 1 ) ) + 1 );
+   b       = modtemp % 10;
+   num     = modtemp / 10;
+
+   if ( a != b )
+   {
+      return false;
+   }
+   else
+   {
+      len -= 2;
+
+      return isPalindrom( num, len );
+   }
+}
+
+int main( ) {
+   unsigned int num;
+
+   printf( " PALINDROM_RECURSION !\nenter a number to check if palindrom or not!\n" );
+   scanf( "%d", &num );
+
+   printf( "Provided number is palindrom? - %d", isPalindrom( num, 5 ) );
+}
