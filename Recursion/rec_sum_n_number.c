ce7dae22fa37014662f47625f421235da19ab57e
Rajkumar <rajkumar.patel@seagate.com>
2021-01-19 11:18:53 +0530

    Add sum of first n numbers using recursion

---
+++ Recursion/rec_sum_n_number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int sum_n( int n ) {
+   if ( n == 0 )
+      return 0;
+
+   return sum_n( n - 1 ) + n;
+}
+
+int main( ) {
+   printf( "SUM N NUMBER USING RECURSION!\n Enter n to find sum of\n" );
+   int n;
+   scanf( "%d", &n );
+
+   printf( "sum of %d is %d", n, sum_n( n ) );
+}
