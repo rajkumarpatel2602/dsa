3cc0e1020a602c1af9e11cb2c3fb0b56c01ac9a7
Rajkumar Patel <rajkumar.patel2602@gmail.com>
2024-09-29 18:54:25 +0200

    Update programLogic.c

--- embedded_concepts/programLogic.c
+++ embedded_concepts/programLogic.c
@@ -335,3 +335,35 @@ void insertion_sort(int a[], int n)
         a[j + 1] = item;
     }
 }
+
+preorder(mynode *root)
+{
+    if(root)
+    {
+        printf("Value : [%d]", root->value);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+Postorder
+postorder(mynode *root)
+{
+    if(root)
+    {
+        postorder(root->left);
+        postorder(root->right);
+        printf("Value : [%d]", root->value);
+        }
+    }
+}
+Inorder
+inorder(mynode *root)
+{
+    if(root)
+    {
+        inorder(root->left);
+        printf("Value : [%d]", root->value);
+        inorder(root->right);
+    }
+}
+Time complexity of traversals is O(n).
