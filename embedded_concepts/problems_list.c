fd850a67c5412073d0ef65422193c65a31c83526
Rajkumar Patel <rajkumar.patel2602@gmail.com>
2024-10-06 16:54:11 +0200

    Update problems_list.c

--- embedded_concepts/problems_list.c
+++ embedded_concepts/problems_list.c
@@ -1,3 +1,20 @@
+//mugging up
+Write C code to solve the Tower of Hanoi problem.
+Here is an example C program to solve the Tower Of Hanoi problem...
+main()
+{
+    towers_of_hanio(n,'L','R','C');
+}
+towers_of_hanio(int n, char from, char to, char temp)
+{
+    if(n>0)
+    {
+        tower_of_hanio(n-1, from, temp, to);
+        printf("\nMove disk %d from %c to %c\n", n, from, to);
+        tower_of_hanio(n-1, temp, to, from);
+    }
+}
+
 // convert from little endian to big endian.
 int myreversefunc(int num)
 {
